c9e50e956f63e8beac99a4c78c2787dcf30c29c5
PiotrZukowski <piozuk011@gmail.com>
2017-01-30 10:30:12 +0100

    8

--- losowanie.php
+++ losowanie.php
@@ -1,3 +1,20 @@
 <?php
-echo "Wylosuj ucznia do odpowiedzi";
+echo "Wylosuj ucznia do odpowiedzi: <br />";
+echo <<<KONIEC
+<form>
+<select name="klasa" >
+	<option>2Ti</option>
+	<option>3Ti</option>
+</select>
+
+<select name="grupa" >
+	<option>Grupa_1</option>
+	<option>Grupa_2</option>
+</select>
+<input type="text" name="numerek" value="" /><br />
+<input type="radio" name="typLosowania" value="rand"/>random
+<input type="radio" name="typLosowania" value="rand"/>random_mt
+
+</form>
+KONIEC;
 ?>

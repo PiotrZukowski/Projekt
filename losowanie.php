a721a227eb987bfea423d35070f5b709b6fd255e
PiotrZukowski <piozuk011@gmail.com>
2017-02-02 11:50:11 +0100

    18

--- losowanie.php
+++ losowanie.php
@@ -1,7 +1,11 @@
 <?php
+srand(time()); // zarodnik, powoduje większa losowość
+mt_srand(time());
+
 $los = "tak";
 if(isset($_REQUEST["los"]))$los = $_REQUEST["los"];
 if ($los == "nie"){
+	$numerek = $_REQUEST["numerek"];
 echo "Wylosuj ucznia do odpowiedzi: nie  <br />";
 echo <<<KONIEC
 <form action="index.php?strona=losowanie" method="POST" >
@@ -14,7 +18,7 @@ echo <<<KONIEC
 	<option>Grupa_1</option>
 	<option>Grupa_2</option>
 </select>
-<input type="text" name="numerek" value="" placeholder="Szczesliwy numerek" /><br />
+<input type="text" name="numerek" value="$numerek" placeholder="Szczesliwy numerek" /><br />
 <input type="radio" name="typLosowania" checked="checked" value="random"/>random
 <input type="radio" name="typLosowania" value="mt_rand"/>mt_rand <br />
 <input type="hidden" name="los" value="tak" />
@@ -26,6 +30,7 @@ KONIEC;
 else if($los=="tak")
 	
 {
+	$numerek = $_REQUEST["numerek"];
 echo "Wylosuj ucznia do odpowiedzi: tak <br />";
 echo <<<KONIEC
 <form action="index.php?strona=losowanie" method="POST" >
@@ -38,7 +43,7 @@ echo <<<KONIEC
 	<option>Grupa_1</option>
 	<option>Grupa_2</option>
 </select>
-<input type="text" name="numerek" value="" placeholder="Szczesliwy numerek" /><br />
+<input type="text" name="numerek" value="$numerek" placeholder="Szczesliwy numerek" /><br />
 <input type="radio" name="typLosowania" checked="checked" value="random"/>random
 <input type="radio" name="typLosowania" value="mt_rand"/>mt_rand <br />
 <input type="hidden" name="los" value="tak" />
@@ -62,7 +67,8 @@ function losujUcznia($class,$group,$number,$type)
 		$g1_3ti = Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
 		do
 		{
-			$i = rand(0, count($g1_3ti )- 1);
+			if($type == "rand") $i = rand(0, count($g1_3ti )- 1);
+			else $i = mt_rand(0, count($g1_3ti )- 1);
 			$x = $g1_3ti[$i];
 		}while($x==$number);
 		
@@ -72,7 +78,8 @@ function losujUcznia($class,$group,$number,$type)
 		$g2_3ti = Array(16,17,18,19,20,21,22,23,24,25,26,27,28,29);
 		do
 		{
-			$i = rand(0, count($g2_3ti )- 1);
+			if($type == "rand") $i = rand(0, count($g2_3ti )- 1);
+			else $i = mt_srand(0, count($g2_3ti )- 1);
 			$x = $g2_3ti[$i];
 		}while($x==$number);
 	}
@@ -81,7 +88,8 @@ function losujUcznia($class,$group,$number,$type)
 		$g1_2ti = Array(3,4,6,7,11,15,16,18,19,20,22,24,25,28,29,32,33); 
 		do
 		{
-			$i = rand(0, count($g1_2ti )- 1);
+			if($type == "rand") $i = rand(0, count($g1_2ti )- 1);
+			else $i = mt_rand(0, count($g1_2ti )- 1);
 			$x = $g1_2ti[$i];
 		}while($x==$number);
 		
@@ -91,7 +99,8 @@ function losujUcznia($class,$group,$number,$type)
 		$g2_2ti = Array(0,1,5,8,9,10,12,13,14,17,21,23,26,27,30,34,35);
 		do
 		{
-			$i = rand(0, count($g2_2ti )- 1);
+			if($type == "rand") $i = rand(0, count($g2_2ti )- 1);
+			else $i = mt_rand(0, count($g2_2ti )- 1);
 			$x = $g2_2ti[$i];
 		}while($x==$number);
 	}

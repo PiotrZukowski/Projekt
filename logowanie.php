15856e0b5b2464f8d20e0f6f9da18d6055f086c2
PiotrZukowski <piozuk011@gmail.com>
2017-02-06 10:40:56 +0100

    23

--- logowanie.php
+++ logowanie.php
@@ -1,7 +1,11 @@
 <?php session_start();
 if(isset($_SESSION["zalogowany"]))$zalogowany = $_SESSION["zalogowany"];
 else $zalogowany = "no";
+if (isset($_REQUEST["test"]))$test = $REQUEST["test"];
+else $test = "nie";
+if ($test == "tak")$zalogowany = zapytajBaze();
 //jeżeli zalogowany
+
 if ($zalogowany == "yes")
 {
 	
@@ -48,15 +52,26 @@ function formularzLogowania()
 {
 	echo <<<END
 	<br />
-	<form action="index.php" method="POST">
+	<form action="index.php?strona=logowanie" method="POST">
 	Login: &nbsp; <input type="text" name="login"  value=""/> <br /> <br />
 	Hasło: &nbsp; <input type="password" name="password"  value="" /> <br /><br />
+	<input type="hidden" name="test" value="tak" />
 	<input type="submit" value="Zaloguj" />
 	</form>
 	
 END;
 }
-
+function zapytajBaze()
+{
+	$login = $_REQUEST["login"];
+	$haslo= $_REQUEST["haslo"];
+	$host = "localhost"; $user = "root"; $password = ""; $baza = "korporacja";
+	$connection = mysqli_connect($host,$user,$password,$baza);
+	if ($conenction)
+	{
+		
+	}
+}
 
 
 
